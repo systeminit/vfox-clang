66cf78d2fb947befe8608061a7e5f04be22c46bf
Sunrise <yane@88.com>
2024-06-29 23:46:40 +0800

    Update information

--- metadata.lua
+++ metadata.lua
@@ -7,7 +7,7 @@ PLUGIN.name = "clang"
 --- Plugin version
 PLUGIN.version = "0.1.1"
 --- Plugin homepage
-PLUGIN.homepage = "https://github.com/yanecc/vfox-clang"
+PLUGIN.homepage = "https://github.com/version-fox/vfox-clang"
 --- Plugin license, please choose a correct license according to your needs.
 PLUGIN.license = "Apache 2.0"
 --- Plugin description
@@ -21,18 +21,6 @@ NOTE:
     vfox will not load the plugin and prompt the user to upgrade vfox.
     --]]
 PLUGIN.minRuntimeVersion = "0.5.0"
---[[
-NOTE:
-    If configured, vfox will check for updates to the plugin at this address,
-    otherwise it will check for updates at the global registry.
-
-    If you want use the global registry to distribute your plugin, you can remove this field.
-
-    If you develop a plugin based on the template, which will automatically generate a manifest file by CI,
-    you can set this address to the manifest file address, so that the plugin can be updated automatically.
-
-    --]]
-PLUGIN.manifestUrl = "https://github.com/yanecc/vfox-clang/releases/download/manifest/manifest.json"
 -- Some things that need user to be attention!
 PLUGIN.notes = {
     "",
